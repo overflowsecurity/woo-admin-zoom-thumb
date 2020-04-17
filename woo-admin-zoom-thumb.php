b0560b5da60e836f77ee7b4b6d2ecc36b78537a9
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-04-17 19:59:09 -0400

    Changed image size to single-post-thumbnail

--- woo-admin-zoom-thumb.php
+++ woo-admin-zoom-thumb.php
@@ -36,7 +36,7 @@ function jt_populate_image( $column_name ) {
 	if( $column_name  == 'jtimage' ) {
 		// if you suppose to display multiple brands, use foreach();
         $x = $image = wp_get_attachment_image_src( get_post_thumbnail_id( $loop->post->ID ), 'thumbnail' );
-        $y = $image = wp_get_attachment_image_src( get_post_thumbnail_id( $loop->post->ID ), 'medium' );
+        $y = $image = wp_get_attachment_image_src( get_post_thumbnail_id( $loop->post->ID ), 'single-post-thumbnail' );
 		echo '<div id="app"><a href="#greeting-modal"><img src=' . $x[0] . '></a>
                 <div id="app">
 
